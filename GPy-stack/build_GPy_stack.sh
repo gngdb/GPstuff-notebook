c618253b07a5b1e722691809e6b37f2967ea2148
Gavin <gavingray1729@gmail.com>
2015-09-11 19:46:28 +0100

    first version of stack that will build

--- GPy-stack/build_GPy_stack.sh
+++ GPy-stack/build_GPy_stack.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-for PYTHONVER in 2 3 ; do
-  PIP="pip$PYTHONVER"
-  REQ="requirements.txt.$PYTHONVER"
-  $PIP install --upgrade pip
+# most recent scipy
+git clone -q git://github.com/scipy/scipy.git
+cp /tmp/scipy-site.cfg scipy/site.cfg
 
+for PYTHONVER in 2 3 ; do
+  PYTHON="python$PYTHONVER"
+  PIP="pip$PYTHONVER"
+  $PIP install --upgrade pip
+  $PIP install --upgrade cython
+  # remove old scipy
+  $PIP uninstall --yes scipy
+
+  (cd scipy && $PYTHON setup.py install)
   $PIP install nose
-  $PIP install gpy
-  $PIP install gpyopt
+  $PIP install six
+  $PIP install --no-deps git+https://github.com/SheffieldML/GPy.git
+  $PIP install --no-deps git+https://github.com/SheffieldML/GPyOpt.git
 done
 
 # Reduce the image size
 apt-get autoremove -y
 apt-get clean -y
+
+cd /
+rm -rf /tmp/build
